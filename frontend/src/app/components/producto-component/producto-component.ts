903c81895f419c028622993b74b6048908631c85
Jose Rico <josearudeveloper@gmail.com>
2025-08-03 21:07:51 +0200

    Fixed: Darkmode and Add: ScrollTop

--- frontend/src/app/components/producto-component/producto-component.ts
+++ frontend/src/app/components/producto-component/producto-component.ts
@@ -1,17 +1,17 @@
-import { Component, input, InputSignal } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { DataView } from 'primeng/dataview';
-import { Tag } from 'primeng/tag';
 import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-producto-component',
-  imports: [DataView, Tag, ButtonModule, CommonModule, RouterModule],
+  imports: [DataView, ButtonModule, CommonModule, RouterModule],
   templateUrl: './producto-component.html',
   styleUrl: './producto-component.css',
 })
 export class ProductoComponent {
-  layout: InputSignal<'grid' | 'list'> = input<'grid' | 'list'>('grid');
+  layout = input<'grid' | 'list'>('grid');
 
   product = input<any>([{
     id: '1000',
@@ -27,19 +27,4 @@ export class ProductoComponent {
 
   options: string[] = ['list', 'grid'];
 
-  getSeverity(product: any) {
-    switch (product.inventoryStatus) {
-      case 'INSTOCK':
-        return 'success';
-
-      case 'LOWSTOCK':
-        return 'warn';
-
-      case 'OUTOFSTOCK':
-        return 'danger';
-
-      default:
-        return null;
-    }
-  }
 }
