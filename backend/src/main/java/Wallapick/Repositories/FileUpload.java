c644f661c2f1e012651ce598f25cdfdf2a961f86
Óscar Sánchez <oscarsanchezro102@gmail.com>
2025-09-19 10:12:44 +0200

    Delete backend/src/main/java/Wallapick/Repositories/FileUpload.java

--- backend/src/main/java/Wallapick/Repositories/FileUpload.java
+++
@@ -1,8 +0,0 @@
-package Wallapick.Repositories;
-
-import Wallapick.Models.Product;
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FileUpload {
-    Product upload(Long id, MultipartFile file);
-}
