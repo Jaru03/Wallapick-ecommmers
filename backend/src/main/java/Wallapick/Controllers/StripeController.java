b84632f0a0bf26e235256c1b4f53c4ff71622aea
Óscar Sánchez <oscarsanchezro102@gmail.com>
2025-09-19 12:26:17 +0200

    Update StripeController.java

--- backend/src/main/java/Wallapick/Controllers/StripeController.java
+++ backend/src/main/java/Wallapick/Controllers/StripeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/stripe")
+@RequestMapping("/stripe")
 public class StripeController {
 
     @Autowired
